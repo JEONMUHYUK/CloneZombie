060af5b7652a9f95419c961a878ab3e27995b430
MUHYUK JEON <67819713+JEONMUHYUK@users.noreply.github.com>
2022-08-29 21:26:44 +0900

    Initial commit

---
+++ Assets/Scripts/GunData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+// menuName : 메뉴상의 경로, fileName : 기본 파일명 , order = 메뉴상의 순서
+[CreateAssetMenu(menuName = "Scriptable/GunData", fileName ="Gun Data", order = 0)]
+public class GunData : ScriptableObject
+{
+    public AudioClip shotClip; // 발사 소리
+    public AudioClip reloadClip; // 재장전 소리
+
+    public float damage = 25; // 공격력
+
+    public int startAmmoRemain = 100; // 처음에 주어질 전체 탄약
+    public int magCapacity = 25; // 탄창 용량
+
+    public float timeBetFire = 0.12f; // 총알 발사 간격
+    public float reloadTime = 1.8f; // 재장전 소요 시간
+}
