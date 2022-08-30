6dd92515f0b97ec59f0155cdb63295c09857f728
MUHYUK JEON <67819713+JEONMUHYUK@users.noreply.github.com>
2022-08-30 10:12:46 +0900

    Finish

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -3,16 +3,16 @@ using UnityEngine.AI; // 내비메쉬 관련 코드
 
 // 주기적으로 아이템을 플레이어 근처에 생성하는 스크립트
 public class ItemSpawner : MonoBehaviour {
-    public GameObject[] items; // 생성할 아이템들
-    public Transform playerTransform; // 플레이어의 트랜스폼
+    public GameObject[] items;                  // 생성할 아이템들
+    public Transform    playerTransform;        // 플레이어의 트랜스폼
 
-    public float maxDistance = 5f; // 플레이어 위치로부터 아이템이 배치될 최대 반경
+    public float        maxDistance = 5f;       // 플레이어 위치로부터 아이템이 배치될 최대 반경
 
-    public float timeBetSpawnMax = 7f; // 최대 시간 간격
-    public float timeBetSpawnMin = 2f; // 최소 시간 간격
-    private float timeBetSpawn; // 생성 간격
+    public float        timeBetSpawnMax = 7f;   // 최대 시간 간격
+    public float        timeBetSpawnMin = 2f;   // 최소 시간 간격
+    private float       timeBetSpawn;           // 생성 간격
 
-    private float lastSpawnTime; // 마지막 생성 시점
+    private float       lastSpawnTime;          // 마지막 생성 시점
 
     private void Start() {
         // 생성 간격과 마지막 생성 시점 초기화
